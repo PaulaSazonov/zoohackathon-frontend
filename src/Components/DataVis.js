ad10aed18504d99b6002409ca898d37452b5f06b
PaulaSazonov <paula.sazonov@gmail.com>
2019-11-09 18:11:46 +0200

    Initialized DataVis-component

---
+++ src/Components/DataVis.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+export default class SearchForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchHitContext: '',
+            link: '',
+        };
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return(
+            <Container>
+
+            </Container>
+        )
+    }
+}
