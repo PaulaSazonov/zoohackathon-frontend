e7d2b0aa5b774d8d8cdaed21cde5ea310268b55c
ainokainen <aino.rouvinen@gmail.com>
2019-11-09 17:22:42 +0200

    Footer and Header displayed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Stylesheets/App.css';
 
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+
 function App() {
   return (
     <div>
+    <Header/>
      This is our app wrapper
+     <Footer/>
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div>
+     This is our Header
+    </div>
+  );
+}
+
+export default Header;
