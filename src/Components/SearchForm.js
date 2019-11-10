502c0cd9e4da68dc9adc694f4ac1eb2601fe075f
PaulaSazonov <paula.sazonov@gmail.com>
2019-11-10 10:38:54 +0200

    Search button styling

--- src/Components/SearchForm.js
+++ src/Components/SearchForm.js
@@ -1,85 +1,79 @@
-import React, { Component } from 'react';
-import {
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  Button,
-  Container,
-  Spinner
-} from 'reactstrap';
-import { GetSearchResults } from '../ServiceClient';
-import DataVis from './DataVis';
-import '../Stylesheets/SearchForm.css';
+import React, { Component } from 'react'
+import { Form, FormGroup, Label, Input, Button, Container, Spinner } from 'reactstrap'
+import { GetSearchResults } from '../ServiceClient'
+import DataVis from './DataVis'
+import '../Stylesheets/SearchForm.css'
 
 export default class SearchForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchUrl: '',
-      searchWord: '',
-      isProcessing: false,
-      searchResults: []
-    };
-  }
-
-  handleChange = e => {
-    const id = e.target.id;
-    const value = e.target.value;
-
-    if (id === 'searchUrl') {
-      this.setState({ searchUrl: value });
-    } else {
-      this.setState({ searchWord: value });
+    constructor(props) {
+        super(props)
+        this.state = {
+            searchUrl: '',
+            searchWord: '',
+            isProcessing: false,
+            searchResults: [],
+        }
     }
-  };
 
-  handleClick = async () => {
-    this.setState({ isProcessing: true });
-    let searchParameters = this.state;
-    console.log('search params: ', searchParameters);
+    handleChange = e => {
+        const id = e.target.id
+        const value = e.target.value
 
-    const results = await GetSearchResults(searchParameters);
-    console.log('results', results);
-    this.setState({ searchResults: results, isProcessing: false });
-  };
-
-  render() {
-    let searchResults = '';
-    if (this.state.isProcessing) {
-      return <Spinner color='primary' />;
-    } else {
-      searchResults = this.state.searchResults;
+        if (id === 'searchUrl') {
+            this.setState({ searchUrl: value })
+        } else {
+            this.setState({ searchWord: value })
+        }
     }
-    return (
-      <Container>
-        <Form>
-          <FormGroup>
-            <Label for='searchUrl'>URL</Label>
-            <Input
-              type='text'
-              name='searchUrl'
-              id='searchUrl'
-              placeholder='Insert URL'
-              onChange={this.handleChange}
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for='searchWord'>Search Word</Label>
-            <Input
-              type='text'
-              name='searchWord'
-              id='searchWord'
-              placeholder='Insert search word'
-              onChange={this.handleChange}
-            />
-          </FormGroup>
-        </Form>
-        <Button color="success" size="lg" onClick={this.handleClick}>
-          Search
-        </Button>
-        <DataVis searchResults={searchResults} />
-      </Container>
-    );
-  }
+
+    handleClick = async () => {
+        this.setState({ isProcessing: true })
+        let searchParameters = this.state
+        console.log('search params: ', searchParameters)
+
+        const results = await GetSearchResults(searchParameters)
+        console.log('results', results)
+        this.setState({ searchResults: results, isProcessing: false })
+    }
+
+    render() {
+        let searchResults = ''
+        if (this.state.isProcessing) {
+            return <Spinner color="success" />
+        } else {
+            searchResults = this.state.searchResults
+        }
+        return (
+            <Container>
+                <Form>
+                    <FormGroup>
+                        <Label for="searchUrl">URL</Label>
+                        <Input
+                            type="text"
+                            name="searchUrl"
+                            id="searchUrl"
+                            placeholder="Insert URL"
+                            onChange={this.handleChange}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="searchWord">Search Word</Label>
+                        <Input
+                            type="text"
+                            name="searchWord"
+                            id="searchWord"
+                            placeholder="Insert search word"
+                            onChange={this.handleChange}
+                        />
+                    </FormGroup>
+                </Form>
+                <div className="col text-center">
+                    <Button id="searchBtn" size="lg" onClick={this.handleClick}>
+                        Search
+                    </Button>
+                </div>
+                <DataVis searchResults={searchResults} />
+            </Container>
+        )
+    }
 }
